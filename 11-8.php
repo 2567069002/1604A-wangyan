f50068afd6aabe0a90214d7c1875a9c6f25e65ff
wangyan <42994345+2567069002@users.noreply.github.com>
2018-11-08 18:28:20 +0800

    11.8
    11.8

---
+++ 11-8.php
@@ -0,0 +1,21 @@
+<?php
+function Student($n,$m)
+{
+    echo "<pre>";
+    $a = 1;
+    $arr = range(0,$n-1);
+    while(true){
+        if(count($arr)==1){
+            return array_keys($arr)[0];
+        }
+        foreach($arr as $key=>$val){
+            $a++;
+            if($a==$m-1){
+                unset($arr[$key]);
+                $a=0;
+            }
+        }
+    }
+}
+
+echo Student(10,30);
