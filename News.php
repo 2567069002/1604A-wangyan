27ee131c314700df2db46a011b4b45528678ee48
wangyan <42994345+2567069002@users.noreply.github.com>
2018-11-19 15:24:48 +0800

    周考三
    周考三

---
+++ News.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $coment
+ * @property string $addtime
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['coment'], 'string'],
+            [['addtime'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'coment' => 'Coment',
+            'addtime' => 'Addtime',
+        ];
+    }
+}
