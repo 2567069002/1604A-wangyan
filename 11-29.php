775f57da3c72d8ab14efc1fd6483b8d0fb088577
wangyan <42994345+2567069002@users.noreply.github.com>
2018-11-29 08:55:44 +0800

    11.29
    11.29

---
+++ 11-29.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ming
+ * Date: 2018/11/29
+ * Time: 8:19
+ */
+
+$arr = array(1,2,3,4,5);
+
+$arr1 =array(6,7,8);
+
+//echo array_sum($arr);
+
+//echo array_shift($arr);
+
+//echo array_unshift($arr,6);
+
+//echo array_pop($arr);
+
+//echo array_push($arr,7);
+
+//print_r(array_values($arr));
+
+//echo count($arr);
+
+//var_dump(is_array($arr));
+
+//print_r(array_merge($arr,$arr1));
+
+//print_r(array_keys($arr));
+
+$a = ' Abcdefg';
+
+//echo strlen($a);
+
+//print_r(str_replace($a,'a','2'));
+
+//echo implode($arr);
+
+//echo str_repeat($a,2);
+
+//echo trim($a);
+
+//echo str_pad($a,10,1);
+
+//echo strrev($a);
+
+//echo strpos($a,a);
+
+//echo chunk_split($a);
+
+//echo strtolower($a);
+
+//echo strtoupper($a);
